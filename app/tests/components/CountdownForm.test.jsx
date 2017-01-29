cfd4f75ce13ef52e853d0db6d5b0585b2e5d8e19
qwertypomy <qwertypomy@gmail.com>
2017-01-29 13:33:42 +0200

    Add countdown functionality

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -2,7 +2,7 @@ var expect = require('expect');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
-import $ from 'jquery';
+var $ = require('jQuery');
 
 var CountdownForm = require('CountdownForm');
 
